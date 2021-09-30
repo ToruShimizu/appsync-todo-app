5db5c39d70d3320649320c7a35e7b1ff2362ea7f
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-30 21:18:53 +0900

    fix: nuxt-appで自動で生成されるページの修正

---
+++ layouts/no-header.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'LayoutsNoHeader',
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.container.fill-height {
+  justify-content: center;
+}
+</style>

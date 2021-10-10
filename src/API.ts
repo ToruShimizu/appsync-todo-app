156527a9d7aa89972f6b211c834caf446b1620d8
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-10-10 22:00:38 +0900

    fix: #32 スキーマ更新

--- src/API.ts
+++ src/API.ts
@@ -2,303 +2,249 @@
 /* eslint-disable */
 //  This file was automatically generated and should not be edited.
 
-export type CreateTodoInput = {
-  userId: string,
-  id?: string | null,
-  name: string,
-  description?: string | null,
+export type CreateTaskInput = {
+  userId: string;
+  name: string;
+  description?: string | null;
 };
 
-export type Todo = {
-  __typename: "Todo",
-  user?: User,
-  id?: string,
-  name?: string,
-  description?: string | null,
-  createdAt?: string,
-  updatedAt?: string,
+export type Task = {
+  __typename?: 'Task';
+  userId: string;
+  id?: string;
+  name?: string;
+  description?: string | null;
+  createdAt?: string;
+  updatedAt?: string;
 };
 
-export type User = {
-  __typename: "User",
-  id?: string,
-  username?: string,
-  name?: string,
-  furigana?: string,
+export type UpdateTaskInput = {
+  id: string;
+  name?: string | null;
+  description?: string | null;
 };
 
-export type UpdateTodoInput = {
-  id: string,
-  name?: string | null,
-  description?: string | null,
-};
-
-export type DeleteTodoInput = {
-  id?: string | null,
+export type DeleteTaskInput = {
+  userId?: string | null;
+  id?: string | null;
 };
 
 export type CreateUserInput = {
-  username: string,
-  name: string,
-  furigana: string,
+  username: string;
+};
+
+export type User = {
+  __typename: 'User';
+  id?: string;
+  username?: string;
+  tasks?: Array<Task | null>;
 };
 
 export type UpdateUserInput = {
-  id: string,
-  username?: string | null,
-  name?: string | null,
-  furigana?: string | null,
+  id: string;
+  username: string;
 };
 
 export type DeleteUserInput = {
-  id: string,
+  id: string;
 };
 
-export type CreateTodoMutationVariables = {
-  input?: CreateTodoInput,
+export type CreateTaskMutationVariables = {
+  input?: CreateTaskInput;
 };
 
-export type CreateTodoMutation = {
-  createTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+export type CreateTaskMutation = {
+  createTask: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  };
 };
 
-export type UpdateTodoMutationVariables = {
-  input?: UpdateTodoInput,
+export type UpdateTaskMutationVariables = {
+  input?: UpdateTaskInput;
 };
 
-export type UpdateTodoMutation = {
-  updateTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+export type UpdateTaskMutation = {
+  updateTask: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  };
 };
 
-export type DeleteTodoMutationVariables = {
-  input?: DeleteTodoInput,
+export type DeleteTaskMutationVariables = {
+  input?: DeleteTaskInput;
 };
 
-export type DeleteTodoMutation = {
-  deleteTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+export type DeleteTaskMutation = {
+  deleteTask: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  };
 };
 
 export type CreateUserMutationVariables = {
-  input?: CreateUserInput,
+  input?: CreateUserInput;
 };
 
 export type CreateUserMutation = {
-  createUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
+  createUser: {
+    __typename: 'User';
+    id: string;
+    username: string;
+    tasks: Array<{
+      __typename: 'Task';
+      userId: string;
+      id: string;
+      name: string;
+      description?: string | null;
+      createdAt: string;
+      updatedAt: string;
+    } | null>;
+  };
 };
 
 export type UpdateUserMutationVariables = {
-  input?: UpdateUserInput,
+  input?: UpdateUserInput;
 };
 
 export type UpdateUserMutation = {
-  updateUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
+  updateUser: {
+    __typename: 'User';
+    id: string;
+    username: string;
+    tasks: Array<{
+      __typename: 'Task';
+      userId: string;
+      id: string;
+      name: string;
+      description?: string | null;
+      createdAt: string;
+      updatedAt: string;
+    } | null>;
+  };
 };
 
 export type DeleteUserMutationVariables = {
-  input?: DeleteUserInput,
+  input?: DeleteUserInput;
 };
 
 export type DeleteUserMutation = {
-  deleteUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
+  deleteUser: {
+    __typename: 'User';
+    id: string;
+    username: string;
+    tasks: Array<{
+      __typename: 'Task';
+      userId: string;
+      id: string;
+      name: string;
+      description?: string | null;
+      createdAt: string;
+      updatedAt: string;
+    } | null>;
+  };
 };
 
-export type GetTodoQueryVariables = {
-  id?: string,
+export type GetTasksQueryVariables = {
+  userId?: string;
 };
 
-export type GetTodoQuery = {
-  getTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+export type GetTasksQuery = {
+  getTasks: Array<{
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+};
+
+export type GetTaskQueryVariables = {
+  id?: string;
+};
+
+export type GetTaskQuery = {
+  getTask: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  };
 };
 
 export type GetUserQueryVariables = {
-  id?: string,
+  id?: string;
 };
 
 export type GetUserQuery = {
-  getUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
+  getUser: {
+    __typename: 'User';
+    id: string;
+    username: string;
+    tasks: Array<{
+      __typename: 'Task';
+      userId: string;
+      id: string;
+      name: string;
+      description?: string | null;
+      createdAt: string;
+      updatedAt: string;
+    } | null>;
+  };
 };
 
-export type OnCreateTodoSubscription = {
-  onCreateTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+export type OnCreateTaskSubscription = {
+  onCreateTask?: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
-export type OnUpdateTodoSubscription = {
-  onUpdateTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
+export type OnUpdateTaskSubscription = {
+  onUpdateTask?: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
 
-export type OnDeleteTodoSubscription = {
-  onDeleteTodo?:  {
-    __typename: "Todo",
-    user:  {
-      __typename: "User",
-      id: string,
-      username: string,
-      name: string,
-      furigana: string,
-    },
-    id: string,
-    name: string,
-    description?: string | null,
-    createdAt: string,
-    updatedAt: string,
-  } | null,
-};
-
-export type OnCreateUserSubscriptionVariables = {
-  id?: string | null,
-  username?: string | null,
-  name?: string | null,
-  furigana?: string | null,
-};
-
-export type OnCreateUserSubscription = {
-  onCreateUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
-};
-
-export type OnUpdateUserSubscriptionVariables = {
-  id?: string | null,
-  username?: string | null,
-  name?: string | null,
-  furigana?: string | null,
-};
-
-export type OnUpdateUserSubscription = {
-  onUpdateUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
-};
-
-export type OnDeleteUserSubscriptionVariables = {
-  id?: string | null,
-  username?: string | null,
-  name?: string | null,
-  furigana?: string | null,
-};
-
-export type OnDeleteUserSubscription = {
-  onDeleteUser?:  {
-    __typename: "User",
-    id: string,
-    username: string,
-    name: string,
-    furigana: string,
-  } | null,
+export type OnDeleteTaskSubscription = {
+  onDeleteTask?: {
+    __typename: 'Task';
+    userId: string;
+    id: string;
+    name: string;
+    description?: string | null;
+    createdAt: string;
+    updatedAt: string;
+  } | null;
 };
--- src/graphql/subscriptions.ts
+++ src/graphql/subscriptions.ts
@@ -2,15 +2,10 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateTodo = /* GraphQL */ `
-  subscription OnCreateTodo {
-    onCreateTodo {
-      user {
-        id
-        username
-        name
-        furigana
-      }
+export const onCreateTask = /* GraphQL */ `
+  subscription OnCreateTask {
+    onCreateTask {
+      userId
       id
       name
       description
@@ -19,15 +14,10 @@ export const onCreateTodo = /* GraphQL */ `
     }
   }
 `;
-export const onUpdateTodo = /* GraphQL */ `
-  subscription OnUpdateTodo {
-    onUpdateTodo {
-      user {
-        id
-        username
-        name
-        furigana
-      }
+export const onUpdateTask = /* GraphQL */ `
+  subscription OnUpdateTask {
+    onUpdateTask {
+      userId
       id
       name
       description
@@ -36,80 +26,15 @@ export const onUpdateTodo = /* GraphQL */ `
     }
   }
 `;
-export const onDeleteTodo = /* GraphQL */ `
-  subscription OnDeleteTodo {
-    onDeleteTodo {
-      user {
-        id
-        username
-        name
-        furigana
-      }
+export const onDeleteTask = /* GraphQL */ `
+  subscription OnDeleteTask {
+    onDeleteTask {
+      userId
       id
       name
       description
       createdAt
       updatedAt
-    }
-  }
-`;
-export const onCreateUser = /* GraphQL */ `
-  subscription OnCreateUser(
-    $id: ID
-    $username: String
-    $name: String
-    $furigana: String
-  ) {
-    onCreateUser(
-      id: $id
-      username: $username
-      name: $name
-      furigana: $furigana
-    ) {
-      id
-      username
-      name
-      furigana
-    }
-  }
-`;
-export const onUpdateUser = /* GraphQL */ `
-  subscription OnUpdateUser(
-    $id: ID
-    $username: String
-    $name: String
-    $furigana: String
-  ) {
-    onUpdateUser(
-      id: $id
-      username: $username
-      name: $name
-      furigana: $furigana
-    ) {
-      id
-      username
-      name
-      furigana
-    }
-  }
-`;
-export const onDeleteUser = /* GraphQL */ `
-  subscription OnDeleteUser(
-    $id: ID
-    $username: String
-    $name: String
-    $furigana: String
-  ) {
-    onDeleteUser(
-      id: $id
-      username: $username
-      name: $name
-      furigana: $furigana
-    ) {
-      id
-      username
-      name
-      furigana
     }
   }
 `;
